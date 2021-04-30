3fa0f4d3b2168ecb29dcffc3e3837b3f8d7b0e26
Underscorer-tech <78732817+Underscorer-tech@users.noreply.github.com>
2021-04-30 18:18:44 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,25 @@
+class rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.0001,
+           length: 1
+        }
+        this.pointB = pointB
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+    fly(){
+        this.rope.bodyA = null;
+    }
+    attach(body){
+
+
+this.rope.bodyA=body;
+
+    }
+
+   
+    
+}
